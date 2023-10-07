adf4e8469548a437687cb49cfc12d4d9a186855d
jcarriong <finales.15@gmail.com>
2023-10-06 20:56:46 -0500

    chore: adding eureka client and config client

--- src/main/java/com/nttdata/bc46credit/Bc46CreditApplication.java
+++ src/main/java/com/nttdata/bc46credit/Bc46CreditApplication.java
@@ -2,7 +2,9 @@ package com.nttdata.bc46credit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Bc46CreditApplication {
 
